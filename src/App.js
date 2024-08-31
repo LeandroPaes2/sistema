2692ff84bf6e223378e8d56a3d154b9daccb4f2b
LeandroPaes2 <lelepaes205@gmail.com>
2024-08-31 11:52:10 -0300

    bootstrap

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Pagina from "./componentes/layouts/Pagina";
+function App() {
+  return (
+    <div className="App">
+      <Pagina>
+        <h1>Esta é a página de cadastro de cliente</h1>
+      </Pagina>
+
+      <Pagina>
+        <h1>Esta é a página de cadastro de produto</h1>
+      </Pagina>
+
+      <Pagina>
+        <h1>Esta é a página de cadastro de fornecedores</h1>
+      </Pagina>
+    </div>
+  );
+}
+
+export default App;
